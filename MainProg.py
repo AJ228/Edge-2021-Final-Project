cf20e8da30eb109c4b5a8d1d25f8555990571c0a
AJ228 <87118785+AJ228@users.noreply.github.com>
2021-07-12 11:39:08 +0300

    Update MainProg.py
    Created a function that plays rock-paper-scissors to decide who starts each round. Ready to implement rest of game now.

--- MainProg.py
+++ MainProg.py
@@ -1,5 +1,4 @@
 import random
-#Setting up players each game - completed and works
 class Player():
     def __init__(self,name):
         self.__Name = name
@@ -34,12 +33,62 @@ class Computer(Player):
     def __init__(self,name):
         Player.__init__(self,name)
 
-#Playing the game - work in progress
+#Deciding turns with RPS - tested and works
 def PlayRPS(Player,Computer):
-    PRPS = Player.SetRPS() #Rps = rock-paper-scissors
-    CRPS = random.choice(Rps)
-    
-    
+    First = ""
+    Winner = ""
+    while Winner != ("Player" or "Computer"):
+        Player.SetRPS() #Rps = rock-paper-scissors
+        Computer.SetRPS()
+        PRPS = Player.GetRPS()
+        CRPS = Computer.GetRPS()
+        print(Player.GetName(),"got: ",PRPS)
+        print(Computer.GetName(),"got: ",CRPS)
+
+        while PRPS == CRPS:
+            Player.SetRPS()
+            Computer.SetRPS()
+            PRPS = Player.GetRPS()
+            CRPS = Computer.GetRPS()
+            print(Player.GetName(),"got: ",PRPS) #testing purposes
+            print(Computer.GetName(),"got: ",CRPS)
+
+        if PRPS == "Rock" and CRPS == "Scissors":
+            Winner = "Player"
+        
+        elif CRPS == "Rock" and PRPS == "Scissors":
+            Winner = "Computer"
+
+        elif PRPS == "Paper" and CRPS == "Rock":
+            Winner = "Player"
+
+        elif CRPS == "Paper" and PRPS == "Rock":
+            Winner = "Computer" 
+
+        elif PRPS == "Scissors" and CRPS == "Paper":
+            Winner = "Player"
+
+        elif CRPS == "Scissors" and PRPS == "Paper":
+            Winner = "Computer" 
+    if Winner == "Player":
+        First = Player
+    else:
+        First = Computer
+
+    return(First)       
+
+#Setting up players each game - completed and works
+PName = input("Enter player name: ")
+PName = PName.title()
+CName = input("Enter CPU name: ")
+CName = CName.title()
+CName = CName + "(CPU)"
+CID = CName[-4:-1]
+
+Human = Player(PName)
+Comp = Computer(CName)
+
+#Playing the game - work in progress
 
 Goal = 100 #setting goal score
 PScore = 0 #player scores to test game
@@ -52,4 +101,9 @@ RiskLot = [-10,-8,-6,-4,-4,-4,-4,-4,-2,-2,-2,-2,-2,-2,-1,-1,-1,-1,0,0,0,4,4,4,4,
 GoBigOrGoHomeLot = [-50,-25,25,-10,-10,-10,-10,-10,-10,-10,0,0,0,10,10,10,10,10,10,10,20,20,20,20,20,20,20,20,40,40,40,40,40,50,60,80] #more chances to lose more points than RiskLot but max. point reward is higher
 
 #while GameOver == False:
-    
+
+TurnStarter = PlayRPS(Human,Comp)
+if TurnStarter == Human:
+    print(Human.GetName(),"won so they go first")
+else:
+     print(Comp.GetName(),"won so they go first")   
