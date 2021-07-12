4590e6cb90e928b44b230e7271075138091aca4d
AJ228 <87118785+AJ228@users.noreply.github.com>
2021-07-12 10:26:26 +0300

    Create Player and turn setup.py

---
+++ Player and turn setup.py
@@ -0,0 +1,62 @@
+import random
+class Player():
+    def __init__(self,name):
+        self.__Name = name
+        self.__Turns = 0
+        self.__Score = 0
+        self.__Rps = ""
+
+    def GetName(self):
+        return(self.__Name)
+
+    def UpdateTurns(self):
+        self.__Turns = self.__Turns + 1
+
+    def GetTurns(self):
+        return(self.__Turns)
+
+    def UpdateScore(self,addition):
+        self.__Score = self.__Score + addition
+
+    def GetScore(self):
+        return(self.__Score)
+
+    def SetRPS(self):
+        Rps = ["Rock","Paper","Scissors"]
+        self.__Rps = random.choice(Rps)
+
+    def GetRPS(self):
+        return(self.__Rps)
+    
+
+class Computer(Player):
+    def __init__(self,name):
+        Player.__init__(self,name)
+    
+PName = input("Enter player name: ")
+PName = PName.title()
+CName = input("Enter CPU name: ")
+CName = CName.title()
+CName = CName + "(CPU)"
+CID = CName[-4:-1]
+
+#Test codes
+Human = Player(PName)
+Comp = Computer(CName)
+
+print(Human.GetName())
+Human.UpdateTurns()
+print(Human.GetName() + " turn count: ",Human.GetTurns())
+Human.UpdateScore(3)
+print("Human score: ",Human.GetScore())
+Human.SetRPS()
+print("Human got: ",Human.GetRPS())
+      
+print(Comp.GetName())
+Comp.UpdateTurns()
+print(Comp.GetName() + " turn count: ",Comp.GetTurns())
+Comp.UpdateScore(3)
+print("CPU score: ",Comp.GetScore())
+Comp.SetRPS()
+print("CPU got: ",Comp.GetRPS())
+print(CID)
